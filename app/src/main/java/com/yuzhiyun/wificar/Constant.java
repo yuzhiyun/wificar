eb8257e993a66b56d193aac057fdc9ce1e13e2ae
yuzhiyun <2723364262@qq.com>
2016-07-03 14:13:32 +0800

    first
    暑期实习，无线巡航小车安卓端源代码，可控制小车移动以及摄像头移动，并且实时传输小车摄像头视频流到安卓端

---
+++ app/src/main/java/com/yuzhiyun/wificar/Constant.java
@@ -0,0 +1,9 @@
+package com.yuzhiyun.wificar;
+
+import android.content.Context;
+import android.os.Handler;
+
+public class Constant {
+  public static Context context;
+  public static Handler handler=null;
+}
